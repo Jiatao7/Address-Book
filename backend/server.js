1c885d828f1b1e4b187d84557928810955a4c45b
Jiatao Geng <jiataogeng7@gmail.com>
2025-03-23 17:32:31 -0400

    Set up cors

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 //Modules
 const express = require('express')
+const cors = require('cors')
 const mongoose = require('mongoose')
 const addressRoutes = require('./routes/contacts')
 require("dotenv").config()
@@ -8,6 +9,7 @@ require("dotenv").config()
 const app = express()
 
 //Middleware
+app.use(cors())
 app.use(express.json())
 app.use((req, res, next) => {
     console.log(req.path, req.method)
